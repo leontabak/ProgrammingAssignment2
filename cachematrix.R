008159cad5a9a74bd80381755da2bef7a7f359ad
Leon Tabak <l.tabak@ieee.org>
2014-08-22 09:24:45 -0500

    started writing comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,19 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+# Create two functions: 
+#  (1) makeCacheMatrix() is a constructor---it makes a matrix
+#  (2) cacheSolve() computes (or retrieves a previously computed) inverse
+#      of a matrix created with makeCacheMatrix()
+
 ## Write a short comment describing this function
 
+# makeCacheMatrix is a function that can take a matrix
+# from its caller and return to its caller a list of
+# accessor ("getters") and mutator ("setters") functions.
+#    The default value of the parameter is a 1x1 matrix whose single element is NA.
+#    We will not check that the matrix is invertible (numeric, square, non-singular),
+#    but that's what we are hoping to get!
 makeCacheMatrix <- function( someMatrix = matrix()) {
   matrixInverse <- NULL
   
